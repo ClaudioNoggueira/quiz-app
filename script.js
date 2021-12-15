4b3226bdd4b73be7f35362fdba76fcf2adb6bdaa
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2021-12-15 16:00:39 -0300

    Quiz loading

--- script.js
+++ script.js
@@ -79,4 +79,36 @@ const quizData = [
         d: "Veia",
         correct: "d"
     }
-]
+];
+
+const questionElement = document.getElementById('question')
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById("button");
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz];
+
+    questionElement.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+submitBtn.addEventListener("click", () => {
+    currentQuiz++;
+
+    if (currentQuiz < quizData.length) {
+        loadQuiz();
+    } else {
+        //TODO: Show results
+        alert('Quiz respondido!');
+    }
+});
